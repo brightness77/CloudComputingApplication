4c57f986f2f49b8d569d9feaa11cbd3205efb52c
brightness77 <brightness77@foxmail.com>
2023-02-13 16:14:58 -0800

    mp4 ex e

--- MP4_Docker/MP4_HadoopMapReduce_Template/JavaTemplate/PopularityLeague.java
+++ MP4_Docker/MP4_HadoopMapReduce_Template/JavaTemplate/PopularityLeague.java
@@ -34,6 +34,47 @@ public class PopularityLeague extends Configured implements Tool {
     @Override
     public int run(String[] args) throws Exception {
         //TODO
+
+        // Job a is to get all popularity count
+        Configuration conf = this.getConf();
+        FileSystem fs = FileSystem.get(conf);
+        Path tmpPath = new Path("./tmp");
+        fs.delete(tmpPath, true);
+
+        Job jobA = Job.getInstance(conf, "Link Counts");
+        jobA.setOutputKeyClass(IntWritable.class);
+        jobA.setOutputValueClass(IntWritable.class);
+
+        jobA.setMapperClass(LinkCountMap.class);
+        jobA.setReducerClass(LinkCountReduce.class);
+
+        FileInputFormat.setInputPaths(jobA, new Path(args[0]));
+        FileOutputFormat.setOutputPath(jobA, tmpPath);
+
+        jobA.setJarByClass(PopularityLeague.class);
+        jobA.waitForCompletion(true);
+
+        // Job b is to get league and rank them
+        Job jobB = Job.getInstance(conf, "Popularity League");
+        jobB.setOutputKeyClass(IntWritable.class);
+        jobB.setOutputValueClass(IntWritable.class);
+
+        jobB.setMapOutputKeyClass(NullWritable.class);
+        jobB.setMapOutputValueClass(IntArrayWritable.class);
+
+        jobB.setMapperClass(PopularityLeagueMap.class);
+        jobB.setReducerClass(PopularityLeagueReduce.class);
+        jobB.setNumReduceTasks(1);
+
+        FileInputFormat.setInputPaths(jobB, tmpPath);
+        FileOutputFormat.setOutputPath(jobB, new Path(args[1]));
+
+        jobB.setInputFormatClass(KeyValueTextInputFormat.class);
+        jobB.setOutputFormatClass(TextOutputFormat.class);
+
+        jobB.setJarByClass(PopularityLeague.class);
+        return jobB.waitForCompletion(true) ? 0 : 1;
+
     }
 
     public static class IntArrayWritable extends ArrayWritable {
@@ -67,4 +108,157 @@ public class PopularityLeague extends Configured implements Tool {
     }
 
     //TODO
+
+    public static class LinkCountMap extends Mapper<Object, Text, IntWritable, IntWritable> {
+        //TODO
+
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            //TODO
+
+            String line = value.toString();
+            int indexOfLinks = line.indexOf(":");
+
+            Integer curWeb = Integer.parseInt(line.substring(0, indexOfLinks));
+            String[] curLinks = line.substring(indexOfLinks + 2).split(" ");
+
+            // emit intermediate values
+            IntWritable one = new IntWritable(1);
+            IntWritable link = new IntWritable(0);
+            for(String curLink: curLinks) {
+                Integer curParsedLink = Integer.parseInt(curLink);
+                if (curLink.equals(curParsedLink)) {
+                    //exclude self linking condition
+                    continue;
+                }
+                link.set(curParsedLink);
+                context.write(link, one);
+            }
+
+            // write self
+            IntWritable zero = new IntWritable(0);
+            IntWritable source = new IntWritable(curWeb);
+            context.write(source, zero);
+
+            //context.write(<IntWritable>, <IntWritable>); // pass this output to reducer
+        }
+    }
+
+    public static class LinkCountReduce extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+        //TODO
+
+        @Override
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            //TODO
+
+            Integer totalLinkCount = 0;
+            for(IntWritable count: values){
+                totalLinkCount += count.get();
+            }
+
+            IntWritable countWritable = new IntWritable(totalLinkCount);
+            context.write(key, countWritable);
+
+            //context.write(<IntWritable>, <NullWritable>); // print as final output
+        }
+    }
+
+    public static class PopularityLeagueMap extends Mapper<Text, Text, NullWritable, IntArrayWritable> {
+
+        List<Integer> leagueSites;
+
+        @Override
+        protected void setup(Context context) throws IOException,InterruptedException {
+            Configuration conf = context.getConfiguration();
+
+            String leaguePath = conf.get("league");
+
+            String[] leagueStrings = readHDFSFile(leaguePath, conf).split("\n");
+            this.leagueSites = new ArrayList<>();
+            for(String league: leagueStrings){
+                leagueSites.add(Integer.parseInt(league));
+            }
+
+        }
+
+
+        private List<Integer[]> leagueList = new ArrayList<>();
+
+
+        @Override
+        public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+
+            Integer count = Integer.parseInt(value.toString());
+            Integer webId = Integer.parseInt(key.toString());
+
+            if(leagueSites.contains(webId)){
+                leagueList.add(new Integer[]{webId, count});
+            }
+        }
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            //TODO
+
+            for (Integer[] item: leagueList) {
+                IntArrayWritable writable = new IntArrayWritable(item);
+                context.write(NullWritable.get(), writable);
+            }
+
+            //Cleanup operation starts after all mappers are finished
+            //context.write(<NullWritable>, <TextArrayWritable>); // pass this output to reducer
+        }
+    }
+
+
+    public static class PopularityLeagueReduce extends Reducer<NullWritable, IntArrayWritable, IntWritable, IntWritable> {
+
+        @Override
+        protected void setup(Context context) throws IOException,InterruptedException {
+            Configuration conf = context.getConfiguration();
+        }
+        //TODO
+
+        private List<Integer[]> leagueList = new ArrayList<>();
+
+        @Override
+        public void reduce(NullWritable key, Iterable<IntArrayWritable> values, Context context) throws IOException, InterruptedException {
+            //TODO
+
+            for(IntArrayWritable intPair: values){
+                IntWritable[] pair = (IntWritable[]) intPair.toArray();
+                Integer webId = pair[0].get();
+                Integer curCount = pair[1].get();
+
+                leagueList.add(new Integer[]{webId, curCount});
+            }
+
+            //O(n^2) to get rank
+            List<Integer[]> leagueRank = new ArrayList<>();
+            for(Integer[] sourceLeague: leagueList){
+                Integer curRank = 0;
+
+                for(Integer[] compareLeague: leagueList){
+                    if(sourceLeague[0].equals(compareLeague[0])){
+                        //no compare to self
+                        continue;
+                    }
+
+                    if(sourceLeague[1] > compareLeague[1]){
+                        ++curRank;
+                    }
+                }
+
+                leagueRank.add(new Integer[]{sourceLeague[0], curRank});
+            }
+
+            //output rank
+            for(Integer[] league: leagueRank){
+                IntWritable webIdWritable = new IntWritable(league[0]);
+                IntWritable rankIdWritable = new IntWritable(league[1]);
+                context.write(webIdWritable, rankIdWritable);
+            }
+        }
+
+    }
 }
